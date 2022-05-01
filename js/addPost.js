842834fae63cb36dab29295b179f5abde42aef4f
jason06286 <dj4871114@gmail.com>
2022-05-01 23:26:37 +0800

    feat post.js addPost.js

--- js/addPost.js
+++ js/addPost.js
@@ -1,33 +1,127 @@
 const contentDom = document.querySelector("#content");
 const submitBtn = document.querySelector("#btn");
+const fileInput = document.querySelector("#file");
+const imagesDom = document.querySelector("#images");
 
-const url = "https://stormy-plateau-16452.herokuapp.com/posts";
+const url = "https://protected-refuge-81301.herokuapp.com/posts";
+
+let images = [];
 
 submitBtn.addEventListener("click", (e) => {
-  if (!contentDom.value) return alert("請輸入內容!!");
+  if (!contentDom.value)
+    return createToastMessage(false, "請輸入您的貼文內容!!");
   upLoadPost();
+});
+fileInput.addEventListener("change", (e) => {
+  fileToFormData();
 });
 
 async function upLoadPost() {
+  submitBtn.children[0].classList.add("hidden");
+  submitBtn.children[1].classList.remove("hidden");
   const { value } = contentDom;
   try {
     const res = await axios.post(url, {
-      userName: "蠟筆小新",
-      // 貼文者姓名，必填
-      userPhoto:
-        "https://storage.googleapis.com/vue-course-api.appspot.com/supergems/1650355390909.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=OYVx2C6Ux2ukJD9RsvkNGZ%2FtZUxAMhCHOHoF4aP8rNqkIlVipHhHiBiM7Zy%2B44wI%2Bes%2BpvNXaI5t%2BmuELFQ8OWHWoso7QKwJMhiQseyPLr6Bt7RI2UcorBuqWeDF8E1aNJx2UsqyWYUgYEh8WQjndtlBu2qrZp31mTzflLKfHW1OiNBwVWtefC9UQInZzjz9nt0RxE4vK6Q3QZsCtBdp%2FqvSmYtrBqj7pwTt6gWfMQ%2BWamCI%2F5dxC1TKkzZNGIqP6v2tQp87j0Ln6Ka4alTihzZHr1i0iMJM5BcHoyvmpGV1du7rMRHkK%2FfHAgU%2BrTkuHYn8M9Mw23HB1%2B27Xvq7YA%3D%3D",
-      // 貼文者圖片
-      discussContent: value,
-      // 貼文，必填
-      discussPhoto:
-        "https://storage.googleapis.com/vue-course-api.appspot.com/supergems/1650354324377.png?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=dSzg6SiXf539bQUM%2BBl0U0A%2BanoITFRrwDIF%2FAoO2LP8SWAwsguyZuCsZbGbK1S9RzxD%2BOoaxazxJFHVjyaXw2bBvle2AKEqsOvsiqJS4FN3JuS%2Bjx39QZJGf0O2xXxx2QKuey4VjbcYWpcAWRunCwb5XnCqTHpLTj6ipnltJnQrJEkX70QC0N8Z26uFHC1SVpV1r2rjoGUu9UnLEJmdv8EAQdmGGeq8zzGPDvKOFbtsw6K2ZO%2B8UepPoHetQZ%2BA3wiDXyObFUrEkatVBahxx4Pt3Cxap3Sx5gQgybD%2BdW3zsEBPnop24xao9NIGEr%2BkBUUAymx%2BGot%2FY9ybEp18Bg%3D%3D",
-      // 貼圖
+      //   user: "626e3eb476e0041982fc8e78",
+      user: "626e3f3176e0041982fc8e7a",
+      //   user: "626e3f1076e0041982fc8e79",
+      image: images,
+      content: value,
     });
     contentDom.value = "";
-    res.data.status === "success"
-      ? alert("新增貼文成功!!")
-      : alert("新增貼文失敗");
+    images = [];
+    imgRender();
+    res.data.success
+      ? createToastMessage(res.data.success, "新增貼文成功!")
+      : createToastMessage(res.data.success, "新增貼文失敗!");
   } catch (error) {
     console.log(error);
   }
+  submitBtn.children[0].classList.remove("hidden");
+  submitBtn.children[1].classList.add("hidden");
+}
+
+function fileToFormData() {
+  fileInput.setAttribute("disabled", "true");
+
+  const formData = new FormData();
+  const file = fileInput.files[0];
+
+  formData.append("image", file);
+  uploadImgur(formData);
+}
+
+async function uploadImgur(formData) {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "https://api.imgur.com/3/image",
+      data: formData,
+      headers: {
+        Authorization: "Client-ID 9deffd8e1209efa",
+      },
+      mimeType: "multipart/form-data",
+    });
+
+    images.push(res.data.data.link);
+    imgRender();
+    fileInput.removeAttribute("disabled");
+
+    createToastMessage(res.data.success, "新增照片成功!!");
+  } catch (error) {
+    console.log(error);
+    createToastMessage(false, error);
+  }
+}
+
+function imgRender() {
+  const str = images
+    .map(
+      (item) => `<div class="relative group cursor-pointer ">
+    <button
+      type="button"
+      
+      data-id="${item}"
+      class="close group-hover:flex border rounded-full w-6 h-6 hidden justify-center items-center border-black hover:bg-red-400 hover:text-yellow-50 absolute -top-2 -right-2 bg-white"
+    >
+      <span class="material-icons pointer-events-none"> close </span>
+    </button>
+    <img
+    referrerpolicy="no-referrer"
+      class="object-contain w-full"
+      src="${item}"
+      alt=""
+    />
+  </div>`
+    )
+    .join("");
+  imagesDom.innerHTML = str;
+
+  const closeBtns = document.querySelectorAll(".close");
+
+  closeBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      deletImg(e.target.dataset.id);
+    });
+  });
+}
+
+function deletImg(id) {
+  const index = images.findIndex((item) => item === id);
+  images.splice(index, 1);
+  imgRender();
+  createToastMessage(true, "刪除照片成功!!");
+}
+
+function createToastMessage(success, text) {
+  Toastify({
+    text: text,
+    className: "info",
+    close: true,
+    style: {
+      background: success
+        ? "linear-gradient(to right, #00b09b, #96c93d)"
+        : "linear-gradient(to right, #C63300, #880000)",
+    },
+  }).showToast();
 }
--- js/post.js
+++ js/post.js
@@ -1,64 +1,109 @@
 const postsDom = document.querySelector("#posts");
+const loadingDom = document.querySelector("#loading");
+const selectDom = document.querySelector("#select");
+const inputDom = document.querySelector("#search-input");
+const searchBtn = document.querySelector("#search-btn");
 
-const url = "https://stormy-plateau-16452.herokuapp.com/posts";
+searchBtn.addEventListener("click", (e) => {
+  selectDom.value = "desc";
+  getData();
+});
+inputDom.addEventListener("keyup", (e) => {
+  console.log(e);
+  if (e.key === "Enter") {
+    selectDom.value = "desc";
+    getData();
+  }
+});
+selectDom.addEventListener("change", (e) => {
+  getData();
+});
 
+const swiperClass = [];
 let data = [];
 
 getData();
 
 async function getData() {
+  const url = `https://protected-refuge-81301.herokuapp.com/posts?timeSort=${selectDom.value}&q=${inputDom.value}`;
+
+  postsDom.classList.add("hidden");
+  loadingDom.classList.remove("hidden");
   const res = await axios.get(url);
-  data = res.data.data;
-  render();
+  if (res.data.success) {
+    data = res.data.data;
+    render();
+  }
+
+  postsDom.classList.remove("hidden");
+  loadingDom.classList.add("hidden");
 }
 
 function render() {
-  const str = data
-    .map((item) => {
-      if (item.discussPhoto) {
-        return `
-            <div class="w-[533px] border-2 border-black rounded-lg p-6 card-shandow bg-white mb-4">
-            <div class="flex items-center mb-4">
-                <img src="${item.userPhoto}"
-                    alt="" class="w-[45px] h-[45px] mr-4">
-                <div>
-                    <h2 class="font-bold">${item.userName}</h2>
-                    <p class="text-gray-400 text-xs">${formatTime(
-                      item.createAt
-                    )}</p>
-                </div>
-            </div>
-            <p class="font-normal mb-4">${formatContent(
-              item.discussContent
-            )}</p>
-            <div class="rounded-lg overflow-hidden h-[157px]">
-                <img src="${item.discussPhoto}"
-                    alt="">
-            </div>
-        </div>
-            `;
-      } else {
-        return `
-            <div class="w-[533px] border-2 border-black rounded-lg p-6 card-shandow bg-white mb-4">
-            <div class="flex items-center mb-4">
-                <img src="${item.userPhoto}"
-                    alt="" class="w-[45px] h-[45px] mr-4">
-                <div>
-                    <h2 class="font-bold">${item.userName}</h2>
-                    <p class="text-gray-400 text-xs">${formatTime(
-                      item.createAt
-                    )}</p>
-                </div>
-            </div>
-            <p class="font-normal mb-4">
-            ${formatContent(item.discussContent)}
-            </p>
-        </div>
-            `;
-      }
-    })
-    .join("");
+  let str = "";
+  if (data.length === 0) {
+    str = `    <div class="w-[533px] border-2 border-black rounded-lg  card-shandow bg-white mb-4">
+    <div class="flex items-center mb-4  gap-x-2 border-b-2 border-black p-4">
+        <div class="w-2 h-2 rounded-full bg-red-500"></div>
+        <div class="w-2 h-2 rounded-full bg-yellow-500"></div>
+        <div class="w-2 h-2 rounded-full bg-green-500"></div>
+    </div>
+    <p class="font-normal mb-4 text-center text-gray-400 py-5">
+      目前無動態，新增一則貼文吧!
+    </p>
+</div> `;
+    postsDom.innerHTML = str;
+    return;
+  }
+  data.forEach((item) => {
+    let str2 = "";
+    let str1 = `
+    <div class="w-[533px] border-2 border-black rounded-lg p-6 card-shandow bg-white mb-4">
+      <div class="flex items-center mb-4">
+          <img src="${item.user.photo}"
+              alt="${item.user.name}" class="w-[45px] h-[45px] mr-4 ">
+          <div>
+              <h2 class="font-bold">${item.user.name}</h2>
+              <p class="text-gray-400 text-xs">${formatTime(item.createdAt)}</p>
+          </div>
+      </div>
+      <p class="font-normal mb-4">
+          ${formatContent(item.content)}
+      </p>
+
+    `;
+    if (item.image.length === 0) {
+      str2 = `</div> `;
+    } else if (item.image.length === 1) {
+      str2 = ` <div class="rounded-lg overflow-hidden h-[157px]">
+      <img src="${item.image[0]}"
+          alt="${item.content}">
+  </div>
+</div>
+      `;
+    } else {
+      str2 =
+        `<div class="swiper swiper-${item._id}">
+      <div class="swiper-wrapper">` +
+        item.image
+          .map(
+            (image) =>
+              ` <div class="swiper-slide"><img src="${image}" alt="貼文圖片"></div>
+        `
+          )
+          .join("") +
+        `</div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+      </div>
+</div>`;
+      swiperClass.push(`.swiper-${item._id}`);
+    }
+    str += str1 + str2;
+  });
   postsDom.innerHTML = str;
+  createSwiper();
 }
 
 function formatTime(time) {
@@ -67,4 +112,21 @@ function formatTime(time) {
 
 function formatContent(str) {
   return str.replace(/\n/g, "<br>");
+}
+
+function createSwiper() {
+  swiperClass.forEach((item) => {
+    new Swiper(item, {
+      cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+    });
+  });
 }
